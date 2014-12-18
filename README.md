8171eeee250cbbae400e009a473ea2488cb75b90
Karl Stoney <karl.stoney@hp.com>
2014-12-18 12:38:07 +0000

    Implemented the rest of the REST stuff

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A simple object based interface to SQL data sources.  Maybe this will evolve into an ORM in the future, but probably not as there are some great ORMs for none-mssql databases available already, and I'm not going to be doing enough with mssql to justify creating one.
 
+The other nice thing this exposes is a wrapper for [restify](https://www.npmjs.com/package/restify), to enable you to create REST routes for your given tables.
+
 This project is simply designed to make interaction with SQL database tables a little simpler, in a nice `when` promise based library.
 
 Currently supported drivers are:
@@ -118,6 +120,43 @@ table.deleteMany()
 });
 ```
 
+## REST
+To make life even easier, you can wrap a restify server with a table object, which will in turn expose the rest routes.  
+
+NOTE: This is NOT a complete solution, i've done a quick pragmatic implementation to enable an on going project.
+```
+// Create your restify server
+var server = require('restify').createServer({
+  name: 'osql-rest',
+  version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+// Define your table
+var table = new osql.Table({
+  table: 'example_table',
+  identity: 'id',
+  database: db
+});
+
+// Wrap the server
+table.wrap('/api', server);
+
+// Profit
+server.listen(port, done);
+```
+From there, you will get a bunch of URLs exposed on the server:
+```
+POST /example_table
+GET /example_table
+GET /example_table/id
+PUT /example_table/id
+DELETE /example_table/id
+```
+Have a look in test/rest.test.js to see how to query it using Restler
+
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
@@ -136,6 +175,7 @@ In lieu of a formal styleguide, take care to maintain the existing coding style.
  - 0.2.2 table.options.identity is no longer a required paramter as tables dont always have identities
  - 0.2.4 Version Bump
  - 0.2.5 Changed a console warn to a log, so as not to break jenkins pipelines
+ - 0.2.6 REST wrapping for Restify
 
 ## License
 Copyright (c) 2014 Karl Stoney  
--- test/rest.test.js
+++ test/rest.test.js
@@ -10,10 +10,10 @@ var rest = require('restler');
 var _ = require('lodash');
 
 /*jshint -W068 */
-describe.only('REST Middleware', function() {
+describe('REST Middleware', function() {
   var table;
   var port = 9284;
-  var baseUrl = 'http://127.0.0.1:' + port;
+  var baseUrl = 'http://127.0.0.1:' + port + '/api';
   var driver = drivers[0];
   var db = new Database(driver.name, driver.opts);
 
@@ -38,12 +38,22 @@ describe.only('REST Middleware', function() {
         identity: 'id',
         database: db
       });
-      table.insert({
-        column1: 'test'
-      });
-      table.wrap(server);
+      table.wrap('/api', server);
       server.listen(port, done);
     });
+  });
+
+  beforeEach(function(done) {
+    table.deleteMany()
+      .where('id > 0')
+      .execute()
+      .then(function() {
+        done();
+      });
+      table.insert({
+        id: 1,
+        column1: 'test'
+      });
   });
 
   after(function(done) {
@@ -56,16 +66,16 @@ describe.only('REST Middleware', function() {
     self.url =  '/example_table' + url;
     self.method = method;
     self.name = name;
-    self.validate = function(data, result) {
-      if(assert) {
-        assert(data, result);
-      }
+    self.validate = function(data, result, done) {
       result.statusCode.should.eql(statusCode);
+      if(assert) {
+        return assert(data, result, done);
+      }
+      done();
     };
     self.go = function(done) {
       rest[method](baseUrl + self.url, data).on('complete', function(data, result) {
-        self.validate(data, result);
-        done();
+        self.validate(data, result, done);
       });
     };
     return self;
@@ -75,23 +85,42 @@ describe.only('REST Middleware', function() {
 
   tests.push(new Test('Get All Records', 'get', '', 200));
 
-  tests.push(new Test('Get a Single Record', 'get', '/1', 200, function(data) {
+  tests.push(new Test('Get a Single Record', 'get', '/1', 200, function(data, result, done) {
     data.column1.should.eql('test'); 
+    done();
   }));
 
-  tests.push(new Test('Create a new Record', 'postJson', '', 201, function(data) {
+  tests.push(new Test('Create a new Record', 'postJson', '', 201, function(data, result, done) {
     data.column1.should.eql('new record'); 
     data.should.have.property('id');
+    done();
   }, {
     column1: 'new record'
   }));
 
-  //tests.push(new Test('Delete a Record', 'delete', '/2', 200));
+  tests.push(new Test('Delete a Record', 'del', '/1', 204, function(data, result, done) {
+    var followUp = new Test('Get a Deleted Record', 'get', '/1', 404);
+    followUp.go(done); 
+  }));
 
+  tests.push(new Test('Update a Record', 'putJson', '/1', 200, function(data, result, done) {
+    var followUp = new Test('Get Updated Record', 'get', '/1', 200, function(data, result, done) {
+      data.column1.should.eql('updated');
+      done(); 
+    });
+    followUp.go(done); 
+  }, {
+    column1: 'updated'
+  }));
 
   _.forEach(tests, function(test) {
     it(test.name + '(' + test.url + ')', function(done) {
       test.go(done);
     });
+  });
+
+  it('Should return 404 when updating a resource which doesnt exist', function(done) {
+    var test = new Test('404', 'putJson', '/123', 404, null, { column1: 'test' });
+    test.go(done);
   });
 });
