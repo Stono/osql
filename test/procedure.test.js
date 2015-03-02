19b6d1b9412cf23134fe65b0baf61d0755d1354f
Dharmaraj Ramasamy <dharmaraj.ramasamy@hp.com>
2015-03-02 23:59:10 +0530

    Update procedure call as per refactored code, also added some negative test cases

--- test/procedure.test.js
+++ test/procedure.test.js
@@ -2,7 +2,6 @@
 
 var Database = require('../lib/database');
 var drivers = require('./drivers');
-var mssql = require('mssql');
 var should = require('should');
 
 var getDriver = function(name, opts) {
@@ -16,67 +15,130 @@ before(function(done) {
   db.execute(driver.dropSP1)
     .then(function(){
       db.execute(driver.createSP1);
-    });
-  db.execute(driver.dropSP2)
+    })
+    .then(function(){
+      db.execute(driver.dropSP2);
+    })
     .then(function(){
       db.execute(driver.createSP2);
-    });
-  db.execute(driver.dropSP3)
+    })
+    .then(function(){
+      db.execute(driver.dropSP3);
+    })
     .then(function(){
       db.execute(driver.createSP3);
+      done();
     });
-  done();
 });
 
 after(function(done) {
-  db.execute(driver.dropSP1);
-  db.execute(driver.dropSP2);
-  db.execute(driver.dropSP3);
-  done();
+  db.execute(driver.dropSP1)
+    .then(function(){
+      db.execute(driver.dropSP2);
+    })
+    .then(function(){
+      db.execute(driver.dropSP3);
+        done();
+    });
 });
 
 describe('Stored Procedure', function() {
 
   it('should return record set', function(done) {
-      db.procedure()
-        .then(function(request){
-          request.execute('SP_TEST1', function(err, recordSet){
-            if(err){
-              done(err);
-            }
-            should.exist(recordSet[0][0]);
-            done();
-          });
-        });
-  });  
+      db.procedure.execute('SP_TEST1', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        should.exist(recordSet[0][0]);
+        done();
+      });
+    });  
 
   it('should return record for the given id', function(done) {
-      db.procedure()
-        .then(function(request){
-          request.input('id', mssql.Int(5), 5);
-          request.execute('SP_TEST2', function(err, recordSet){
-            if(err){
-              done(err);
-            }
-            recordSet[0][0].name.should.equal('test5');
-            done();
-          });
-        });
+      db.procedure.input('id', 'Int', 5);
+      db.procedure.execute('SP_TEST2', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        recordSet[0][0].name.should.equal('test5');
+        done();
+      });
   });  
 
   it('should return output for the given id', function(done) {
-      db.procedure()
-        .then(function(request){
-          request.input('id', mssql.Int(5), 7);
-          request.input('result', mssql.NVarChar(50));
-          request.execute('SP_TEST3', function(err, recordSet){
-            if(err){
-              done(err);
-            }
-            recordSet.returnValue.should.equal(0);
-            done();
-          });
-        });
+      db.procedure.input('id', 'Int', 7);
+      db.procedure.output('result', 'NVarChar');
+      db.procedure.execute('SP_TEST3', function(err, recordSet, returnValue){
+        if(err){
+          done(err);
+        }
+        returnValue.should.equal(0);
+        done();
+      });
   });    
- 
+});
+
+
+
+describe('Stored Procedure', function() {
+
+  it('should through error if not a valid stored procedure', function(done) {
+      db.procedure.execute('SP_TEST', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+    });  
+
+  it('should through error if not a valid input parameter', function(done) {
+      db.procedure.input('invalid', 'Int', 5);
+      db.procedure.execute('SP_TEST2', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+  });  
+
+  it('should through error if not a valid data type in input parameter', function(done) {
+      db.procedure.input('id', 'Undefined', 5);
+      db.procedure.execute('SP_TEST2', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+  });     
+
+  it('should through error if less number of parameter passed', function(done) {
+      db.procedure.execute('SP_TEST2', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+  });      
+
+  it('should through error if more number of parameter passed', function(done) {
+      db.procedure.input('id1', 'Int', 5);
+      db.procedure.input('id2', 'Int', 5);
+      db.procedure.execute('SP_TEST2', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+  });    
+
+  it('should return output for the given id', function(done) {
+      db.procedure.input('id', 'Int', 7);
+      db.procedure.output('result', 'NVarChar');
+      db.procedure.execute('SP_TEST3', function(err, recordSet){
+        if(err){
+          done(err);
+        }
+        console.log(recordSet);
+      });
+  });    
 });
